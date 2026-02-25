ab34e336808a73a420f070a70470f1cf0073accf
Sarah Lee <sarah89lee@Gmail.com>
2026-02-25 14:41:56 -0800

    Initial Commit

---
+++ DogPhotosApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DogPhotosApp
+//
+//  Created by Sarah Lee on 2/25/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
